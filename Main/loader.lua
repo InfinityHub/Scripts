fd390713764318e69383057ba44eb0a333db0abb
InfinityHub <143916631+InfinityHub@users.noreply.github.com>
2023-09-04 20:31:17 -0300

    Update loader.lua

--- Main/loader.lua
+++ Main/loader.lua
@@ -13,3 +13,13 @@ if PlaceId == 5780309044 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/InfinityHub/Scripts/main/Games/StandsAwakening/Computer/loader.lua",true))()
     end
 end
+if PlaceId == 11423467063 then
+    if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
+        warn('Mobile Service')
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/InfinityHub/Scripts/main/Games/StandsAwakening/Mobile/Button.lua",true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/InfinityHub/Scripts/main/Games/StandsAwakening/Mobile/Boss.lua",true))()
+    else
+        warn('Computer Service')
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/InfinityHub/Scripts/main/Games/StandsAwakening/Computer/Boss.lua",true))()
+    end
+end
