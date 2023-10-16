ff688fa50ee251b2babd5fb3d85946c0398e11fe
InfinityHub <143916631+InfinityHub@users.noreply.github.com>
2023-10-16 17:29:52 -0300

    Update source.lua

--- Games/ProjectPolaro/Main/source.lua
+++ Games/ProjectPolaro/Main/source.lua
@@ -282,31 +282,3 @@ for _, v in next, teleportNpc do
         end,
     })
 end
-
-
-
--- Anti HttpSpy
-local functions = {rconsoleprint,print,setclipboard,rconsoleerr,rconsolewarn,warn,error}
-for i, v in next, functions do
-    local old
-    old = hookfunction(v, newcclosure(function(...)
-        local args = {...}
-        for i, v in next, args do
-            if tostring(i):find("https") or tostring(v):find("https") then
-                while true do
-                end
-            end
-        end
-        return old(...)
-    end))
-end
-if _G.ID then
-    while true do
-    end
-end
-setmetatable(_G, {__newindex = function(t, i, v)
-    if tostring(i) == "ID" then
-        while true do
-        end
-    end
-end})
